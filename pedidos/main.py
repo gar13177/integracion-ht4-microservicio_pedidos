e840da4549972fd3903cfd05a0a81bae1cc1acbc
Kevin Garcia <gar13177@uvg.edu.gt>
2017-09-04 16:31:13 -0600

    inicio de sesion

--- pedidos/main.py
+++ pedidos/main.py
@@ -4,6 +4,7 @@ from colas.colas import Colas
 from servicios import atender_inicio_de_sesion, atender_ordenes, atender_promociones
 
 from rabbitmq.PromotionServer import get_promotions
+from rabbitmq.LoginRcpServer import new_login
 
 
 if __name__ == "__main__":
@@ -14,9 +15,8 @@ if __name__ == "__main__":
     #response = fibonacci_rpc.call(30)
     #print(" [.] Got %r" % response)
     try:
-        #thread.start_new_thread(atender_inicio_de_sesion, (colas,))
         thread.start_new_thread(get_promotions)
-        #thread.start_new_thread(atender_promociones, (colas,))
+        thread.start_new_thread(new_login)
     except:
         print "Error: unable to start thread"
 
---
+++ pedidos/rabbitmq/LoginRpcServer.py
@@ -0,0 +1,42 @@
+import pika, json
+from servicios import *
+
+LOGIN_FROM_CLIENT_QUEUE = 'login_from_client_queue'
+
+def on_request(ch, method, props, body):
+    usuario = json.loads(body)
+    print usuario
+
+    data_usuario = {}
+    data_usuario['email'] = usuario['user']
+    data_usuario['contrasena'] = usuario['password']
+
+    try:
+        usuario = Servicios_de_Autenticacion().iniciar_sesion(data_usuario)
+        respuesta = {
+            'message': 'welcome %s, your token is %s'%(usuario.email.user,usuario.token),
+            'token': usuario.token
+        }
+        respuesta = json.dumps(respuesta, ensure_ascii=False).encode('utf8')
+    except Exception as e:
+        respuesta = e.message
+        print e.message
+
+    response = respuesta
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+def new_login():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue=LOGIN_FROM_CLIENT_QUEUE)
+    
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(on_request, queue=LOGIN_FROM_CLIENT_QUEUE)
+
+    channel.start_consuming()
