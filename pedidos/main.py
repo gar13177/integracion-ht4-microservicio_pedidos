3cc6663e31945a580e8e592d3caeb175a2545c2a
Kevin Garcia <gar13177@uvg.edu.gt>
2017-09-04 16:12:45 -0600

    promociones new thread

--- pedidos/main.py
+++ pedidos/main.py
@@ -3,19 +3,19 @@ import thread
 from colas.colas import Colas
 from servicios import atender_inicio_de_sesion, atender_ordenes, atender_promociones
 
+from rabbitmq.PromotionServer import get_promotions
 
-def request_login():
 
 if __name__ == "__main__":
-    colas = Colas()
-    fibonacci_rpc = FibonacciRpcClient()
+    #colas = Colas()
+    #fibonacci_rpc = FibonacciRpcClient()
 
-    print(" [x] Requesting fib(30)")
-    response = fibonacci_rpc.call(30)
-    print(" [.] Got %r" % response)
+    #print(" [x] Requesting fib(30)")
+    #response = fibonacci_rpc.call(30)
+    #print(" [.] Got %r" % response)
     try:
         #thread.start_new_thread(atender_inicio_de_sesion, (colas,))
-        thread.start_new_thread(atender_ordenes, (colas,))
+        thread.start_new_thread(get_promotions)
         #thread.start_new_thread(atender_promociones, (colas,))
     except:
         print "Error: unable to start thread"
