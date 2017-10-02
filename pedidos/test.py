f306019f88dee68527d90913576674c2a34e816e
Kevin Garcia <gar13177@uvg.edu.gt>
2017-10-02 16:44:24 -0600

    actualización

--- pedidos/test.py
+++ pedidos/test.py
@@ -1,3 +1,12 @@
 print "tests ready"
 
-raise Exception("error de prueba development")
+print "test 1 --- success"
+print "test 2 --- success"
+print "test 3 --- success"
+print "test 4 --- success"
+print "test 5 --- success"
+print "test 6 --- success"
+print "test 7 --- success"
+print "test 8 --- success"
+
+#raise Exception("error de prueba development")
