efde2f002aee202fd0a013efbba78cab6022d7ca
Kevin Garcia <gar13177@uvg.edu.gt>
2017-10-07 20:13:58 -0600

    actualización de jenkins

--- jobs/docker_run_main.sh
+++ jobs/docker_run_main.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+docker stop ht4_mic_ped
+docker rm ht4_mic_ped
 docker run -d --name=ht4_mic_ped --net=host ht4_mic_ped python /code/pedidos/main.py
--- jobs/docker_run_tests.sh
+++ jobs/docker_run_tests.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+docker stop ht4_mic_ped
+docker rm ht4_mic_ped
 docker run --name=ht4_mic_ped --net=host ht4_mic_ped python /code/pedidos/test.py
